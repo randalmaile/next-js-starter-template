1f8bac90dbb69b3970dc725f36fce3a0eaf2c7df
randy maile <randy.maile@sharp.com>
2021-03-09 17:32:34 -0800

    adding config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
